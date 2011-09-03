e122f438d9def89e1d35555b541fd51c7b14f07a
Robin Cloutman <rycochet@rycochet.com>
2011-09-03 06:36:37 +0100

    First version of skill event

--- src/main/java/mmo/Skills/MMOSkills.java
+++ src/main/java/mmo/Skills/MMOSkills.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of mmoMinecraft (http://code.google.com/p/mmo-minecraft/).
+ * 
+ * mmoMinecraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package mmo.Skills;
 
 import mmo.Core.MMOPlugin;
@@ -7,13 +23,10 @@ public class MMOSkills extends MMOPlugin {
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void onEnable() {
 		// TODO Auto-generated method stub
-		
 	}
-
 }
