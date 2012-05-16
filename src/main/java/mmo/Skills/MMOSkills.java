26cae4e060f983bd935208a08fb52c80b9a20684
Wulfspider <hello@wulfspider.com>
2012-05-16 11:40:54 -0400

    Updated pom.xml, readme, plugin.yml, and added header.txt. Added automatic license header checking and formatting. Formatted headers to be project specific. A little bit more formatting on misc files.

--- src/main/java/mmo/Skills/MMOSkills.java
+++ src/main/java/mmo/Skills/MMOSkills.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoSkills <http://github.com/mmoMinecraftDev/mmoSkills>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoSkills is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
